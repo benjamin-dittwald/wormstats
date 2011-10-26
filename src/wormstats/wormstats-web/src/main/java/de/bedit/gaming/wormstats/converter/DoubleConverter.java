c8f7648d6ef865b91501006e39d85f775ad3b615
benjamin <benjamin@lespaul>
2011-10-26 08:24:13 +0200

    double

---
+++ src/wormstats/wormstats-web/src/main/java/de/bedit/gaming/wormstats/converter/DoubleConverter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bedit.gaming.wormstats.converter;
+
+import java.text.DecimalFormat;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author benjamin
+ */
+@FacesConverter("doubleConverter")
+public class DoubleConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext facesContext, UIComponent component,
+			String submittedValue) {
+		return Double.parseDouble(submittedValue);
+	}
+
+	@Override
+	public String getAsString(FacesContext facesContext, UIComponent component,
+			Object value) {
+		DecimalFormat df = new DecimalFormat("#.###");
+		return df.format((Double) value);
+	}
+}
