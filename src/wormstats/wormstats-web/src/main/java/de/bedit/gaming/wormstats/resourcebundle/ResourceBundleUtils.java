03b851b26208bad985c2427b09173d7d84545647
benjamin <benjamin@palomino>
2012-03-11 19:30:34 +0100

    resourcebundleutil added

---
+++ src/wormstats/wormstats-web/src/main/java/de/bedit/gaming/wormstats/resourcebundle/ResourceBundleUtils.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bedit.gaming.wormstats.resourcebundle;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author benjamin
+ */
+public class ResourceBundleUtils {
+
+    public static String getResourceBundleString(String messageBundleName, String key) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Locale locale = facesContext.getViewRoot().getLocale();
+        ResourceBundle bundle = ResourceBundle.getBundle(messageBundleName, locale);
+        return bundle.getString(key);
+    }
+}
