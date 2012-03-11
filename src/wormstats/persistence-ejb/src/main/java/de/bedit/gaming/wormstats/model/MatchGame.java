07a26f8f5e328716e287e4139c5be0d6135a7ecc
benjamin <benjamin@palomino>
2012-03-11 17:40:20 +0100

    style rule fix

--- src/wormstats/persistence-ejb/src/main/java/de/bedit/gaming/wormstats/model/MatchGame.java
+++ src/wormstats/persistence-ejb/src/main/java/de/bedit/gaming/wormstats/model/MatchGame.java
@@ -4,20 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import org.eclipse.persistence.annotations.PrivateOwned;
 
 @Entity
