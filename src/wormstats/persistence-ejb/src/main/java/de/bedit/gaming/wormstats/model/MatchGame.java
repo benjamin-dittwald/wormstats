759d7779daf5f564f61dc13d3e1b541d4f1de803
Benjamin Dittwald <bendittwald@gmail.com>
2013-01-19 23:28:20 +0100

    running with jboss but still having persistence issues

--- src/wormstats/persistence-ejb/src/main/java/de/bedit/gaming/wormstats/model/MatchGame.java
+++ src/wormstats/persistence-ejb/src/main/java/de/bedit/gaming/wormstats/model/MatchGame.java
@@ -4,68 +4,84 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javax.persistence.*;
-import org.eclipse.persistence.annotations.PrivateOwned;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "matchGame")
-@NamedQueries( {
-        @NamedQuery(name = "getAllMatchGames", query = "SELECT m FROM MatchGame m"),
-        @NamedQuery(name = "getMatchGameById", query = "SELECT m FROM MatchGame m WHERE m.id = :id"),
-        @NamedQuery(name = "getMatchGameByTimestamp", query = "SELECT m FROM MatchGame m WHERE m.matchDate = :date"),
-        @NamedQuery(name = "getAllMatchGamesByWinnerId", query = "SELECT m FROM MatchGame m WHERE m.winner.id = :id") })
+@NamedQueries({
+		@NamedQuery(name = "getAllMatchGames", query = "SELECT m FROM MatchGame m"),
+		@NamedQuery(name = "getMatchGameById", query = "SELECT m FROM MatchGame m WHERE m.id = :id"),
+		@NamedQuery(name = "getMatchGameByTimestamp", query = "SELECT m FROM MatchGame m WHERE m.matchDate = :date"),
+		@NamedQuery(name = "getAllMatchGamesByWinnerId", query = "SELECT m FROM MatchGame m WHERE m.winner.id = :id") })
 public class MatchGame implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "matchDate", nullable = false)
-    private Date matchDate;
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    @PrivateOwned
-    private List<CompetitorMatchStatistic> competitorMatchStatistics = new ArrayList<CompetitorMatchStatistic>();
-    @OneToOne
-    private Competitor winner;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5104699627834511483L;
 
-    public Date getMatchDate() {
-        return matchDate;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "matchDate", nullable = false)
+	private Date matchDate;
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<CompetitorMatchStatistic> competitorMatchStatistics = new ArrayList<CompetitorMatchStatistic>();
+	@OneToOne
+	private Competitor winner;
 
-    public void setMatchDate(Date matchDate) {
-        this.matchDate = matchDate;
-    }
+	public Date getMatchDate() {
+		return matchDate;
+	}
 
-    public Competitor getWinner() {
-        return winner;
-    }
+	public void setMatchDate(Date matchDate) {
+		this.matchDate = matchDate;
+	}
 
-    public void setWinner(Competitor winner) {
-        this.winner = winner;
-    }
+	public Competitor getWinner() {
+		return winner;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public void setWinner(Competitor winner) {
+		this.winner = winner;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public Date getDate() {
-        return matchDate;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public void setDate(Date date) {
-        this.matchDate = date;
-    }
+	public Date getDate() {
+		return matchDate;
+	}
 
-    public List<CompetitorMatchStatistic> getCompetitorMatchStatistics() {
-        return competitorMatchStatistics;
-    }
+	public void setDate(Date date) {
+		this.matchDate = date;
+	}
 
-    public void setCompetitorMatchStatistics(
-            List<CompetitorMatchStatistic> competitorMatchStatistics) {
-        this.competitorMatchStatistics = competitorMatchStatistics;
-    }
+	public List<CompetitorMatchStatistic> getCompetitorMatchStatistics() {
+		return competitorMatchStatistics;
+	}
+
+	public void setCompetitorMatchStatistics(
+			List<CompetitorMatchStatistic> competitorMatchStatistics) {
+		this.competitorMatchStatistics = competitorMatchStatistics;
+	}
 }
